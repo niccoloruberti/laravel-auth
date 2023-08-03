6a59e06c119ee5f8dbb7ef97faea99f9907f127b
niccoloruberti <nikiruberti@gmail.com>
2023-08-03 18:20:21 +0200

    creato seeder per popolare la tabella projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'link_repository', 'topic', 'slug'];
+
+    public static function generateSlug($name) {
+        
+        return Str::slug($name, '-');
+    }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +14,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=1; $i<=10; $i++) {
+            $project = new Project();
+
+            $project->name = $faker->sentence(3);
+            $project->link_repository = $faker->url();
+            $project->topic = $faker->word();
+            $project->slug = $project->generateSlug($project->name);
+
+            $project->save();
+        }
     }
 }
