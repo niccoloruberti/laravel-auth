db6a582ec9f300d60bf298913ca0e3729d7400a1
niccoloruberti <nikiruberti@gmail.com>
2023-08-03 18:42:21 +0200

    creata la pagina index dell'admin

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1>I miei progetti</h1>
+        </div>
+        <div class="col-12 mt-5">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Link</th>
+                        <th>Strumenti</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($projects as $project)
+                        <tr>
+                            <td>{{ $project->id }}</td>
+                            <td>{{ $project->name }}</td>
+                            <td>{{ $project->link_repository }}</td>
+                            <td>
+                                <a href="#" class="btn btn-sm btn-primary">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                                <a href="#" class="btn btn-sm btn-primary">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <form class="d-inline-block" action="#" method="POST">
+                                    @csrf @method('DELETE')
+                                    <button class="btn btn-sm btn-danger">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controller\Admin\ProjectCOntroller as ProjectCOntroller;
 
 /*
 |--------------------------------------------------------------------------
